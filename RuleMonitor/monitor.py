dc62a4c609eb6c0d9da45000b2ff79078928a06e
isaac-mackey <imackey1415@gmail.com>
2021-03-08 07:59:35 -0800

    adding more rule monitoring files

---
+++ RuleMonitor/monitor.py
@@ -0,0 +1,308 @@
+from .event import Event
+from .rule import Rule
+from .assignment import Assignment
+from .parser import *
+import pdb
+import copy
+import time
+
+class Monitor:
+	def __init__(self, monitorName, rule_file_path):
+		self.name = monitorName
+		self.ruleVector = [readRuleFromTxt(rule_file_path)]
+		self.assignmentVector = []
+		self.currentTime = 0  
+
+	def reset(self):
+		self.assignmentVector = []
+		self.currentTime = 0		
+
+	def monitoringLoop(self, eventstream_file_path):
+
+		eventstream = read_eventstream_from_txt(eventstream_file_path)
+
+		eventstream_length = len(eventstream)
+
+		userInput = ''
+
+		start = time.time()
+
+		i=0
+
+		while ((userInput != 'q') and (i < eventstream_length)):
+
+			if userInput == 'q':
+				break
+
+			if userInput == 'a':
+				self.printAssignments()
+
+			if userInput == 'm':
+				for r in self.ruleVector:
+					self.findMatches(r)
+
+			if userInput == 'n':
+				if i == eventstream_length:
+					print("End of sequence")
+
+				if eventstream[i].eventType == "process":
+					self.handleProcessEvent(eventstream[i])
+
+				for r in self.ruleVector:
+					self.findMatches(r)
+
+				self.removeExpiredData(eventstream[i])
+
+				i += 1
+
+			if userInput == 'f':
+				while (i != eventstream_length):
+					
+					if eventstream[i].eventType == "process":
+						self.handleProcessEvent(eventstream[i])					
+					
+					for r in self.ruleVector:
+						self.findMatches(r)
+
+					self.removeExpiredData(eventstream[i])
+
+					i += 1
+
+			if userInput == 'x':
+				self.removeExpiredData(eventstream[i])
+
+			#print ("Input: ", end='')
+			#userInput = raw_input()
+			userInput = 'f'
+
+		end = time.time()
+
+		number_of_violations = 0
+		output_string = ""
+		for a in self.assignmentVector:
+			if a.typeOfAssignment == 'body':
+				if a.complete:
+					if len(a.missingProcessAtoms) == 0:
+						if len(a.matchingAssignments) == 0:
+							output_string += "Unmatched Body Assignment: \n"+str(a)+"\n"
+							number_of_violations += 1
+						else:
+							output_string += "Matched Body Assignment: \n"+str(a)+"\n"
+
+		output_string = "Number of Violations: "+str(number_of_violations)+"\n"+output_string
+
+		return self.assignmentVector, output_string, (end-start)
+
+	def handleProcessEvent(self, e: Event):
+
+		newAssignments = []
+
+		for rule in self.ruleVector:
+
+			# Check if event is relevant in the rule body
+			for bodyProcessAtom in rule.bodyProcessAtoms:
+				if (e.eventName == bodyProcessAtom.processName):
+					
+					# Create new body assignment
+					variablesDefinedFlags = {x:False for x in rule.bodyVariables}
+
+					values = {}
+					for i,v in enumerate(bodyProcessAtom.variables):
+						values[v] = e.values[i]
+						variablesDefinedFlags[v]=True
+
+					seenProcessAtoms = [];
+					seenProcessAtoms.append(bodyProcessAtom)
+
+					missingProcessAtoms = []
+					for ruleBodyProcessAtom in rule.bodyProcessAtoms:
+						if bodyProcessAtom != ruleBodyProcessAtom:
+							missingProcessAtoms.append(ruleBodyProcessAtom)
+
+					matchingAssignments = []
+
+					new_body_assignment = Assignment(rule.bodyVariables,
+					values,variablesDefinedFlags,"body",
+					rule, missingProcessAtoms,seenProcessAtoms,[],[e])
+
+					self.assignmentVector.append(new_body_assignment)
+					newAssignments.append(new_body_assignment)
+
+			# Check if event is relevant in the rule head
+
+			for headProcessAtom in rule.headProcessAtoms:
+				if (e.eventName == headProcessAtom.processName):
+					
+					# create new head assignment
+					variablesDefinedFlags = {x:False for x in rule.headVariables}
+
+					values = {}
+					for i,var in enumerate(headProcessAtom.variables):
+						values[var] = e.values[i]
+						variablesDefinedFlags[var]=True
+
+					seenProcessAtoms = [];
+					seenProcessAtoms.append(headProcessAtom)
+
+					missingProcessAtoms = []
+					for ruleHeadProcessAtom in rule.headProcessAtoms:
+						if headProcessAtom != ruleHeadProcessAtom:
+							missingProcessAtoms.append(ruleHeadProcessAtom)
+
+					matchingAssignments = []
+
+					new_head_assignment = Assignment(rule.headVariables,
+					values,variablesDefinedFlags,"head",
+					rule, missingProcessAtoms,seenProcessAtoms,[],[e])
+
+					self.assignmentVector.append(new_head_assignment)
+					newAssignments.append(new_head_assignment)
+
+			# loop through existing assignment vector
+			# searching for assignments this event will extend
+			for assignment in self.assignmentVector:
+				assignmentDefinedVariables = []
+
+				for var in assignment.variables:
+					if assignment.variablesDefinedFlags[var]:
+						assignmentDefinedVariables.append(var)
+
+				for missingAtom in assignment.missingProcessAtoms:
+					if (e.eventName == missingAtom.processName):
+						eventValuesForVariables = {}
+
+						for i,var in enumerate(m.variables):
+							eventValuesForVariables[var] = e.values[i]
+
+						match = True
+
+						for var in missingAtom.variables:
+							if assignment.variablesDefinedFlags[var]:
+								if (assignment.values[var] != eventValuesForVariables[var]):
+									match = False;
+									break;
+						
+						if not match:
+							continue;
+
+						if assignment.typeOfAssignment == "body":
+							constraints = assignment.rulePointer.bodyGapAtoms
+						else:
+							constraints = assignment.rulePointer.headGapAtoms
+
+						mergedMap = assignment.values.copy()
+
+						for var in eventValuesForVariables.keys():
+							if not var in assignmentDefinedVariables:
+								assignmentDefinedVariables.append(var)
+							mergedMap[var] = eventValuesForVariables[var]
+
+						# check if new assignment is consistent with assignment of interest
+						consistentWithGapAtoms = True
+
+						for constraint in constraints:
+							if ((constraint.lhs in mergedMap.keys()) and
+							(constraint.rhs in mergedMap.keys())):
+								if not constraint.checkTruthValueWithAssignment(mergedMap):
+									consistentWithGapAtoms = False
+
+						if consistentWithGapAtoms:
+							b = copy.deepcopy(assignment)
+							
+							for var in mergedMap.keys():
+								b.values[var] = mergedMap[var]
+								b.variablesDefinedFlags[var] = True
+
+							for x in b.missingProcessAtoms:
+								if str(x)==str(m):
+									b.missingProcessAtoms.remove(x) 
+
+							b.seenProcessAtoms.append(missingAtom)
+
+							b.seenEvents.append(e)
+
+							b.complete = len(b.missingProcessAtoms)==0
+
+							b.expirationTime = b.computeExpirationTime()
+
+							self.assignmentVector.append(b)
+		
+		return newAssignments
+
+	def findMatches(self, r: Rule):
+		
+		# using body assignment b, find matching head assignment h
+		for b in self.assignmentVector:
+			if ((b.rulePointer.ruleName == r.ruleName) and (b.complete) and b.typeOfAssignment == 'body'):
+				
+				for h in self.assignmentVector:
+					if ((h.rulePointer.ruleName == r.ruleName) and (h.typeOfAssignment == 'head') and (h.complete) and h not in b.matchingAssignments):
+						if(self.doAssignmentsMatch(b,h)):
+
+							# found a match
+							b.matchingAssignments.append(h)
+
+	def expired(self, a: Assignment):
+		return a.expirationTime < self.currentTime
+		
+	# Removing data that expires before time of event e
+	def removeExpiredData(self, e: Event):
+		
+		# this removal is an optimization, ignoring it for now by returning early
+		return
+		
+		latestEventTime = int(e.values[-1])
+
+		unexpiredData = []
+		expiredData = []
+
+		for a in self.assignmentVector:
+			if a.expirationTime < latestEventTime:
+				unexpiredData.append(a)
+			else:
+				expiredData.append(a)
+
+		self.assignmentVector = unexpiredData
+
+	def printAssignments(self):
+		for i,a in enumerate(self.assignmentVector):
+			print("assignmentVector["+str(i)+"]")
+			print(a)
+
+	def doAssignmentsMatch(self, a, b):
+		for v in a.variables:
+			if v in b.variables:
+				if (a.values[v] != b.values[v]):
+					return False
+
+		mergedMap = a.values.copy()
+
+		for k,v in b.values.items():
+			mergedMap[k] = v
+
+		constraints = []
+
+		if (a.typeOfAssignment == 'body'):
+			constraints += a.rulePointer.bodyGapAtoms
+			constraints += b.rulePointer.headGapAtoms
+		else:
+			constraints += a.rulePointer.headGapAtoms
+			constraints += b.rulePointer.bodyGapAtoms
+		
+		for c in constraints:
+			if (not c.checkTruthValueWithAssignment(mergedMap)):
+				return False
+
+		return True
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RuleMonitor/parser.py
@@ -0,0 +1,132 @@
+from .event import Event
+from .rule import Rule
+from .assignment import Assignment
+from .processatom import ProcessAtom
+from .gapatom import GapAtom
+
+import csv
+
+def read_eventstream_from_csv(eventStreamCSVFileName):
+		eventStream = []
+
+		with open(eventStreamCSVFileName, newline='') as f:
+			reader = csv.reader(f)
+			for d in list(reader):
+				if not d:
+					continue
+				eventType = d[0]
+				eventName = d[1]
+				eventData = list(map(lambda y: y.strip(), d[2:]))
+				eventData[-1] = int(eventData[-1])
+				eventStream.append(Event(eventType, eventName, eventData))
+
+		return eventStream
+
+# used with the output format of Gabriel's workflow simulator
+def read_eventstream_from_txt(eventStreamTxtFileName):
+	eventStream = []
+
+	with open(eventStreamTxtFileName, 'r') as f:
+		for d in f.readlines():
+			d = d.strip()
+			if "START" in d or "END" in d:
+				continue
+			# remove _ _ _ numbers
+			d_list = d.split(' ')
+			eventTime = int(d_list[0])
+			d_list.pop(0)
+			d_list.pop(0)
+			d_list.pop(0)
+			eventType = "process"
+			eventName = d_list[0]
+			eventData = list(map(lambda y: y.split("=")[1], d_list[1:]))
+			eventData.append(eventTime)
+			eventStream.append(Event(eventType, eventName, eventData))
+
+	return eventStream
+
+def readRuleFromTxt(ruleTxtFile):
+	file1 = open(ruleTxtFile, 'r')
+	
+	lines = file1.read().splitlines()
+
+	file1.close()
+
+	lines.pop(0) # consume "Rule"
+
+	ruleName = lines.pop(0) # get rule name
+
+	lines.pop(0) # consume "if"		
+	
+	line = lines.pop(0) # get first line after if
+	
+	bodyProcessAtoms = []
+	bodyGapAtoms = []
+	headProcessAtoms = []
+	headGapAtoms = []
+
+	while(line != 'then'):
+		if ("<" in line or "=" in line or ">" in line):
+			bodyGapAtoms.append(parseGapAtomString(line))
+		else:
+			bodyProcessAtoms.append(parseProcessAtomString(line))
+		line = lines.pop(0)
+
+	lines.pop() # remove "end"
+
+	for line in lines:
+		if ("<" in line or "=" in line or ">" in line):
+			headGapAtoms.append(parseGapAtomString(line))
+		else:
+			headProcessAtoms.append(parseProcessAtomString(line))
+
+	r = Rule(ruleName, bodyProcessAtoms, bodyGapAtoms, headProcessAtoms, headGapAtoms)
+	return r
+
+def parseProcessAtomString(s):
+	name = s[:s.find("(")]
+	dataString = s[s.find('(')+1:s.find(')')]
+	attributes = []
+	variables = []
+	for d in dataString.split(','):
+		data = d.strip().split(" ")
+		attributes.append(data[0])
+		variables.append(data[1])
+	attributes.append('time')
+	variables.append(s[s.find('@')+1:])
+
+	return ProcessAtom(name, attributes, variables)
+
+
+def parseGapAtomString(s):
+	lhs = ''
+	rhs = ''
+	gap = 0
+	direction = ''
+	offset = 0
+
+	if '<=' in s:
+		direction = '<='
+		offset = 1
+	elif '>=' in s:
+		direction = '>='
+		offset = 1
+	elif '>' in s:
+		direction = '>'
+	elif '<' in s:
+		direction = '<'
+	elif "=" in s:
+		direction = '='
+	else:
+		print("Operator not recognized")
+
+	rhs = s[s.find(direction)+offset+1:].strip()
+
+	if (s.find('+') != -1):
+		lhs = s[:s.find('+')][:s.find(direction)].strip()
+		gap = s[s.find('+')+1:s.find(direction)-1]
+	else:
+		lhs = s[:s.find(direction)].strip()
+
+	return GapAtom(lhs, rhs, direction, 'days', int(gap))
+
