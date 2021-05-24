5d79222dd377e87c380566a68423d7687fb24b4e
isaac-mackey <imackey1415@gmail.com>
2021-05-24 13:38:18 -0700

    removing access rule, adding request rule

--- input/moonlight/model3/data.py
+++ input/moonlight/model3/data.py
@@ -1,5 +1,5 @@
 from numpy import random
 
 
-def access(input):
+def request(input):
     return {'ticket': {'Class': random.choice(['support', 'trust', 'bug'], p=[0.6, 0.2, 0.2]), 'Value': int(input['ticket']['Value']) + random.randint(0, 100)}}
