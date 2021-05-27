b47680d9e33bc8eeac2bfef186233cf53572f22d
isaac-mackey <imackey1415@gmail.com>
2021-05-27 01:55:53 -0700

    differing models for different process instance lengths

--- input/moonlight/model1/data.py
+++ input/moonlight/model1/data.py
@@ -1,4 +1,10 @@
 from numpy import random
 
+from numpy import random
+
+
 def request(input):
+    return {'ticket': {'Class': random.choice(['support', 'trust', 'bug'], p=[0.6, 0.2, 0.2]), 'Value': int(input['ticket']['Value']) + random.randint(0, 100)}}
+
+def register(input):
     return {'ticket': {'Class': random.choice(['support', 'trust', 'bug'], p=[0.6, 0.2, 0.2]), 'Value': int(input['ticket']['Value']) + random.randint(0, 100)}}
--- input/moonlight/model3/data.py
+++ input/moonlight/model3/data.py
@@ -3,3 +3,6 @@ from numpy import random
 
 def request(input):
     return {'ticket': {'Class': random.choice(['support', 'trust', 'bug'], p=[0.6, 0.2, 0.2]), 'Value': int(input['ticket']['Value']) + random.randint(0, 100)}}
+
+def register(input):
+    return {'ticket': {'Class': random.choice(['support', 'trust', 'bug'], p=[0.6, 0.2, 0.2]), 'Value': int(input['ticket']['Value']) + random.randint(0, 100)}}
